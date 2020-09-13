7a9c93509f66ce091ed0968d3ceae0d66d967ed4
ayaka <asmnkys@gmail.com>
2020-09-13 09:52:33 +0000

    add new file

---
+++ hello.rb
@@ -0,0 +1,36 @@
+puts "hello ruby !!!!!"
+
+# ""まで表示
+p "Branding Engineer"
+p "Hello"
+p "HR Tech"
+
+# ""内が表示
+puts "Branding Engineer"
+puts "Hello"
+puts "HR Tech"
+
+# ""全て一行で表示
+print "Branding Engineer"
+print "Hello"
+print "HR Tech"
+
+
+p 1
+p 2
+p 3
+
+
+
+puts 1
+puts 2
+puts 3
+
+
+
+puts "1"
+puts "2"
+puts "3"
+
+
+
