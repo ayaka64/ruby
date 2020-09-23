fdcfda23fb8c783b624b8780f256f5b0797a76ea
ayaka <asmnkys@gmail.com>
2020-09-23 10:28:17 +0000

    add new file

--- ruby04.rb
+++ ruby04.rb
@@ -1,12 +1,46 @@
 # テキストruby04　課題
 
-# 1.
+# 1.~4.
 class People
+  def initialize
+    p 'Peopleのインスタンスが作られました。'
+  end
+  
+  def self.msg
+    p '私はPeopleクラスです。'
+  end
 end
 
-# 2.
- 
-# 3.
-# 4.
-# 5.
-# 6.
+People.msg
+
+class People
+  def name=(value)
+    @name = value
+  end
+  
+  def name
+    @name 
+  end
+    
+end
+
+class People
+  attr_accessor :name
+end
+
+people = People.new
+people.name = "ayaka"
+p people.name
+
+  
+# 5.~6.
+class People
+  def self.msg
+    p "私は目からビームが出せます"
+  end
+end
+
+class ChildPeople < People
+end
+
+ChildPeople.msg
